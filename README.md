5f18fcec9e54a3ac5c9f67635d52addcbcab134e
herodek <herodek_10@hotmail.com>
2020-07-05 21:59:54 -0300

    Criando footer e navbar

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # NodeTask
+
+
+##Requisitos
+>Tenha instalado na máquina: Node.JS, mongodb e um navegador.
+
+##Inicializar
+>Para inicializar a aplicação basta digitar "NPM Start", a aplicação iniciará na URL: localhost:8080.
+
+##Autor
+>Philipe Herodek Neto - herodek_10@hotmail.com
--- index.js
+++ index.js
@@ -1,16 +1,50 @@
+//Node_modules
 const express = require('express')
+const handlebars = require('express-handlebars')
+const path = require('path')
+const mongoose = require('mongoose')
 
 
+//Configurações
 
-
+//express
 const app = express()
 
 
+//Handlebars
+app.engine('handlebars', handlebars({
+    defaultLayout: 'main',
+    allowedProtoMethods:{
+        trim:true,
+    }
+}))
+app.set('view engine','handlebars')
+
+app.use(express.static(path.join(__dirname,'/public/')))
+
+//Mongoose
+mongoose.connect("mongodb://localhost/nodetask",{useNewUrlParser:true,useUnifiedTopology:true})
+const db = mongoose.connection;
+db.on('err', console.error.bind(console,'Erro na conexão'));
+db.once('open', ()=>{console.log('Banco conectado !')})
+
+
+
+
+//Rotas
 app.get("/", (req,res)=>{
-    res.send("Teste")
+    res.render('home')
 })
 
-const port = 8080
+app.get("/task", (req,res)=>{
+    res.render('./tasks/task')
+})
+
+
+
+//Resto
+
+const port = process.env.PORT || 8080
 app.listen(port, ()=>{
     console.log("Node rodando na porta: "+port)
 })
